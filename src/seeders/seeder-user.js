89bac2fadf9f159459ac37e0e56d2fecabff0c39
mintranno1 <minhtranhcm.mt@gmail.com>
2023-01-01 11:17:51 +0700

    #24 - video 33: Tạo xong các bảng bằng migration và seeder

--- src/seeders/seeder-user.js
+++ src/seeders/seeder-user.js
@@ -4,9 +4,16 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Users', [{
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'example@example.com',
+      email: 'mintran.hcms@gmail.com',
+      password: 'Admin@123',
+      firstName: 'Min',
+      lastName: 'tran',
+      address: 'Ho Chi Minh',
+      gender: 1,  
+      image: 'https://www.image.com',               
+      phoneNumber: '0932507713',   
+      roleId: '',    
+      positionId: '',                  
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
